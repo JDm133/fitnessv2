7209961d2f88b840e3ba07fca9cd461318b164de
lukis <167894877+JDm133@users.noreply.github.com>
2024-09-03 18:13:53 +0200

    logowanie/rejestracja

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/connectDB');
+const authRoutes = require('./routes/auth');
+const cors = require('cors'); // Importowanie pakietu cors
+
+const app = express();
+
+// Używanie CORS
+app.use(cors());
+
+app.use(express.json());
+app.use('/auth', authRoutes);
+
+connectDB();
+
+app.listen(5000, () => {
+    console.log(`Serwer działa na porcie ${process.env.PORT || 5000}`);
+});
---
+++ backend/sesja/sesja.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const sesja = (req, res, next) => {
+    const token = req.header('x-auth-token');
+
+    if (!token) {
+        return res.status(401).json({ message: 'Brak tokenu, autoryzacja odrzucona' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, 'secretkey');
+        req.user = decoded.userId;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Token jest nieprawidłowy' });
+    }
+};
+
+module.exports = sesja;
